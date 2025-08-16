eef609add761d9fc54212b96547f2ffc206f5d9d
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-08-17 00:15:14 +0530

    Creating project setup

---
+++ src/main/java/dev/anuradha/githubreposearcher/GithubRepoSearcherApplication.java
@@ -0,0 +1,13 @@
+package dev.anuradha.githubreposearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoSearcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/anuradha/githubreposearcher/GithubRepoSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anuradha.githubreposearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
