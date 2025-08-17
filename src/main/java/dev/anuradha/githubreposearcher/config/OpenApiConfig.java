71b48bada96c4ff706d6548c79c2c2a4696ec224
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-08-17 17:02:08 +0530

    Added config classes and created beans

--- src/main/java/dev/anuradha/githubreposearcher/config/OpenApiConfig.java
+++ src/main/java/dev/anuradha/githubreposearcher/config/OpenApiConfig.java
@@ -1,4 +1,15 @@
 package dev.anuradha.githubreposearcher.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI api(){
+        return new OpenAPI().info(new Info()
+                .title("GitHub Repository Searcher API")
+                .version("v1")
+        );
+    }
 }
--- src/main/java/dev/anuradha/githubreposearcher/config/WebClientConfig.java
+++ src/main/java/dev/anuradha/githubreposearcher/config/WebClientConfig.java
@@ -1,5 +1,23 @@
 package dev.anuradha.githubreposearcher.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
 
+    @Bean
+    WebClient githubWebClient(
+            @Value("${github.base-url}") String baseUrl,
+            @Value("${github.token:}") String token) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.ACCEPT, "application/vnd.github+json")
+                .defaultHeader(HttpHeaders.USER_AGENT, "github-searcher-app")
+                .defaultHeaders(h -> { if (!token.isBlank()) h.setBearerAuth(token); })
+                .build();
+    }
 }
